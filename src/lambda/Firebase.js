7f45bc771ffa95da7ace47525bda24ba6002b099
Mahfoudh AROUS <aladin02dz@hotmail.fr>
2019-03-11 16:16:32 +0100

    debugging

--- src/lambda/Firebase.js
+++ src/lambda/Firebase.js
@@ -15,6 +15,14 @@ const config = {
     messagingSenderId: messagingSenderId
 };
 
+console.log("---");
+console.log(process.env.apiKey);
+console.log(process.env.authDomain);
+console.log(process.env.databaseURL);
+console.log(process.env.projectId);
+console.log(process.env.storageBucket);
+console.log(process.env.messagingSenderId);
+console.log("---");
 console.log("config="+JSON.stringify(config));
 console.log("---");
 console.log(process.env);
