0f57d837b8f783fda358dce448ebb77d4972e0b6
Mahfoudh AROUS <aladin02dz@hotmail.fr>
2019-03-11 11:19:50 +0100

    starting process environment variables

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -2,10 +2,13 @@
 const obj = {
   foo: 'bar'
 };
+
+const { msg } = process.env;
+
 export function handler(event, context, callback) {
   console.log('queryStringParameters', event.queryStringParameters);
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({ msg: 'Hello, World!', ...obj })
+    body: JSON.stringify({ msg: msg, ...obj })
   });
 }
