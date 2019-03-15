06d55539f2566af24e7a1815921e7876de74e35f
Mahfoudh AROUS <aladin02dz@hotmail.fr>
2019-03-15 19:40:35 +0100

    debugging

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 import 'firebase/auth';
 
+/*
 const config = {
     apiKey: "AIzaSyB4qz-N1SmGnnXePH-p9YE6Z8DCg-P-sI8",
     authDomain: "fir-3-38956.firebaseapp.com",
@@ -10,23 +11,10 @@ const config = {
     storageBucket: "fir-3-38956.appspot.com",
     messagingSenderId: "636417239392"
 };
-
-firebase.initializeApp(config);
-
-async function getconfig(){
-  const response = await fetch('/.netlify/functions/fbconfig')
-  const resObj = await response.json()
-  const fbconfig = await resObj.fbconfig;
-  return fbconfig;
-}
-
-const fbConfig = getconfig();
-
-console.log("firebase37");
-console.log(fbConfig);
+*/
 
 const { REACT_APP_API_KEY, REACT_APP_AUTH_DOMAIN, REACT_APP_DATABASE_URL, REACT_APP_PROJECT_ID, REACT_APP_STORAGE_BUCKET, REACT_APP_MESSAGING_SENDER_ID} = process.env;
-const config2 = {
+const config = {
   apiKey: REACT_APP_API_KEY,
   authDomain: REACT_APP_AUTH_DOMAIN,
   databaseURL: REACT_APP_DATABASE_URL,
@@ -34,30 +22,9 @@ const config2 = {
   storageBucket: REACT_APP_STORAGE_BUCKET,
   messagingSenderId: REACT_APP_MESSAGING_SENDER_ID
 };
-  console.log("-----------------------------");
-  console.log(config2);
+firebase.initializeApp(config);
 
-/*
-var data1;
-const fbconfig = fetch('/.netlify/functions/fbconfig')
-  .then(response => response.json())
-  .then(json => 
-    {
-      data1 = json;
-      return Promise.resolve(json.fbconfig);
-    });
-
-console.log("firebase32");
-console.log("fbConfig="+fbconfig+", data="+data1);
-
-console.log(fbconfig);
-console.log("fbconfig.__proto__");
-console.log(fbconfig.toString());
-console.log("--------------------");
-console.log(fbconfig.valueOf());
-console.log("--------------------");
-console.log(fbconfig.PromiseValue);
-*/
+console.log("firebase38");
 
 export const provider = new firebase.auth.GoogleAuthProvider();
 export const auth = firebase.auth();
