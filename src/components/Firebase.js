f7628b16dfc77722fab64999a504e4a7899ce74a
Mahfoudh AROUS <aladin02dz@hotmail.fr>
2019-03-15 19:35:35 +0100

    debugging

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -22,18 +22,18 @@ async function getconfig(){
 
 const fbConfig = getconfig();
 
-console.log("firebase36");
+console.log("firebase37");
 console.log(fbConfig);
 
-const { API_KEY, AUTH_DOMAIN, DATABASE_URL, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID} = process.env;
-  const config2 = {
-    apiKey: API_KEY,
-    authDomain: AUTH_DOMAIN,
-    databaseURL: DATABASE_URL,
-    projectId: PROJECT_ID,
-    storageBucket: STORAGE_BUCKET,
-    messagingSenderId: MESSAGING_SENDER_ID
-  };
+const { REACT_APP_API_KEY, REACT_APP_AUTH_DOMAIN, REACT_APP_DATABASE_URL, REACT_APP_PROJECT_ID, REACT_APP_STORAGE_BUCKET, REACT_APP_MESSAGING_SENDER_ID} = process.env;
+const config2 = {
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  databaseURL: REACT_APP_DATABASE_URL,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID
+};
   console.log("-----------------------------");
   console.log(config2);
 
