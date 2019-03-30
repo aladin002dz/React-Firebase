92fe6195df9348eb9a91b72982b762716614ac5f
Mahfoudh AROUS <aladin02dz@hotmail.fr>
2019-03-30 19:49:05 +0100

    adding link to online result

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # React-Firebase
 
 Introduction to React with Firebase.
+
+link to result **[here](https://react-firebase-101.netlify.com/)**
--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -2,17 +2,6 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 import 'firebase/auth';
 
-/*
-const config = {
-    apiKey: "AIzaSyB4qz-N1SmGnnXePH-p9YE6Z8DCg-P-sI8",
-    authDomain: "fir-3-38956.firebaseapp.com",
-    databaseURL: "https://fir-3-38956.firebaseio.com",
-    projectId: "fir-3-38956",
-    storageBucket: "fir-3-38956.appspot.com",
-    messagingSenderId: "636417239392"
-};
-*/
-
 const { REACT_APP_API_KEY, REACT_APP_AUTH_DOMAIN, REACT_APP_DATABASE_URL, REACT_APP_PROJECT_ID, REACT_APP_STORAGE_BUCKET, REACT_APP_MESSAGING_SENDER_ID} = process.env;
 const config = {
   apiKey: REACT_APP_API_KEY,
